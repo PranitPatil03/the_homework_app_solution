adab4659469a16f20592d0689d725b7cf7e0a43e
PranitPatil03 <patilpranit3112@gmail.com>
2022-12-21 01:27:16 +0530

    Project Setup Completed

---
+++ frontend/the_homework_app_solution/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Signup from './components/Signup';
+import Start from './components/Start';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <>
+      <Start/>
+      <Signup/>
+      <Login/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/the_homework_app_solution/src/components/Startgame.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Startgame() {
+  return (
+    <div>Startgame</div>
+  )
+}
